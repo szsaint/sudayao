e8906d55f41d4fc38842ef352f1f38728b01f5fa
yzp <yzpniceboy@gmail.com>
2015-10-13 20:16:40 +0800

    初始化提交

---
+++ sudayao/sudayao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sudayao
+//
+//  Created by yyx on 15/10/13.
+//  Copyright © 2015年 saint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sudayao/sudayao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sudayao
+//
+//  Created by yyx on 15/10/13.
+//  Copyright © 2015年 saint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
